d998b20f6f2d58477697af123ba836494650060d
VasiliCheb <87862701+VasiliCheb@users.noreply.github.com>
2023-03-02 15:31:47 +0000

    Deploying to gh-pages from @ VasiliCheb/fitness@735f681594a05829f6a75b405270e9c92668932d 🚀

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import './swiper.js';
-import './video-play.js';
+import {initVideo} from './video-play.js';
 import './tabs.js';
 
 
 const body = document.querySelector('.page__body');
 
 body.classList.remove('page__body--nojs');
+
+initVideo();
--- js/video-play.js
+++ js/video-play.js
@@ -1,17 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+export const initVideo = function () {
+  const tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  const video = document.querySelector('.video');
-  if (video) {
-    const playButton = video.querySelector('.video__play');
-    const preview = video.querySelector('img');
-    const videoSource = video.dataset.source;
-    playButton.addEventListener('click', () => {
-      playButton.remove();
-      preview.remove();
-      video.classList.add('video--played');
-      video.insertAdjacentHTML('afterbegin', '<iframe width="364" height="228" src="' + videoSource + '" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+  let player = document.querySelector('.hall__video-player');
+  let link = document.querySelector('.hall__video-button');
+  let boxVideo = document.querySelector('.hall__video');
+
+  link.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    boxVideo.classList.add('is-show');
+    new YT.Player(player, {
+      videoId: '9TZXsZItgdw',
+      events: {
+        onReady: (e) => e.target.playVideo(),
+      },
     });
-  } else {
-    return;
-  }
-});
+  });
+
+};
